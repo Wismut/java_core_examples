b0ea212d93c99ffb7a2bab080a3c195925d0b98e
Wismut <wismutmax@gmail.com>
2020-04-15 13:47:58 +0300

    Implements InstanceMethWithObjectRefDemo Implements MethodRefDemo2

---
+++ src/main/java/com/wismut/javacore/chapter15/MethodRefDemo2.java
@@ -0,0 +1,36 @@
+package main.java.com.wismut.javacore.chapter15;
+
+interface StringFunc4 {
+    String func(String n);
+}
+
+class MyStringOps2 {
+    String strReverse(String str) {
+        String result = "";
+
+        int i;
+
+        for (i = str.length() - 1; i >= 0; i--) {
+            result += str.charAt(i);
+        }
+        return result;
+    }
+}
+
+public class MethodRefDemo2 {
+    static String stringOp(StringFunc4 sf, String s) {
+        return sf.func(s);
+    }
+
+    public static void main(String[] args) {
+        String inStr = "Lambdas add power to Java";
+        String outStr;
+
+        MyStringOps2 strOps = new MyStringOps2();
+
+        outStr = stringOp(strOps::strReverse, inStr);
+
+        System.out.println("Original string: " + inStr);
+        System.out.println("String reversed: " + outStr);
+    }
+}

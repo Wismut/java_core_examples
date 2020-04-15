daf5fb1ecc520ff957182b515446116225895f6b
sergiofokin <sergiyborysovych@ukr.net>
2020-04-15 23:30:20 +0300

    Implements GenericMethodRefDemo Implements UseMethodRef

---
+++ src/main/java/com/wismut/javacore/chapter15/UseMethodRef.java
@@ -0,0 +1,37 @@
+package main.java.com.wismut.javacore.chapter15;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+class MyClass {
+    private int val;
+
+    MyClass(int v) {
+        val = v;
+    }
+
+    int getVal() {
+        return val;
+    }
+}
+
+public class UseMethodRef {
+    static int compareMC(MyClass a, MyClass b) {
+        return a.getVal() - b.getVal();
+    }
+
+    public static void main(String[] args) {
+        ArrayList<MyClass> a1 = new ArrayList<>();
+
+        a1.add(new MyClass(1));
+        a1.add(new MyClass(4));
+        a1.add(new MyClass(2));
+        a1.add(new MyClass(9));
+        a1.add(new MyClass(3));
+        a1.add(new MyClass(7));
+
+        MyClass maxValObj = Collections.max(a1, UseMethodRef::compareMC);
+
+        System.out.println("Maximum value is: " + maxValObj.getVal());
+    }
+}

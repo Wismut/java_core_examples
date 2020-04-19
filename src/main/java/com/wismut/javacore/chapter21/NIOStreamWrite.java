54d776f54bdad5fe82596187a02ba95d7f85f143
wismut <codename__47@ukr.net>
2020-04-19 19:00:38 +0300

    Implements NIOStreamWrite Implements PathDemo

---
+++ src/main/java/com/wismut/javacore/chapter21/NIOStreamWrite.java
@@ -0,0 +1,23 @@
+package main.java.com.wismut.javacore.chapter21;
+
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
+
+public class NIOStreamWrite {
+    public static void main(String[] args) {
+        try (OutputStream fout = new BufferedOutputStream(Files.newOutputStream(Paths.get("test.txt")))) {
+            for (int i = 0; i < 26; i++) {
+                fout.write((byte) ('A' + i));
+            }
+        } catch (InvalidPathException e) {
+            System.out.println("Path error " + e);
+        } catch (IOException e) {
+            System.out.println("I/O error: " + e);
+        }
+    }
+}

4aa099d26f5e561c3998c5efc3c214c443842d2f
wismut <codename__47@ukr.net>
2020-04-20 13:52:10 +0300

    Implements InetAddressTest Implements URLDemo Implements Whois

---
+++ src/main/java/com/wismut/javacore/package22/URLDemo.java
@@ -0,0 +1,18 @@
+package main.java.com.wismut.javacore.package22;
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class URLDemo {
+    public static void main(String[] args) throws MalformedURLException {
+        URL hp = new URL("http://www.HerbSchildt.com/WhatsNew");
+
+        System.out.println("Protocol: " + hp.getProtocol());
+        System.out.println("Port: " + hp.getPort());
+
+        System.out.println("Host: " + hp.getHost());
+        System.out.println("File: " + hp.getFile());
+        System.out.println("Ext:" + hp.toExternalForm());
+    }
+}

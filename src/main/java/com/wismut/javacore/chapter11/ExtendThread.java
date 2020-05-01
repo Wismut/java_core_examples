5adce42087ef4bf6e51127b701ce2eb66b7ab784
Wismut <wismutmax@gmail.com>
2020-05-01 21:09:55 +0300

    Implements ExtendThread

---
+++ src/main/java/com/wismut/javacore/chapter11/ExtendThread.java
@@ -0,0 +1,38 @@
+package main.java.com.wismut.javacore.chapter11;
+
+class NewThread2 extends Thread {
+    NewThread2() {
+        super("Demo Thread");
+        System.out.println("Child thread: " + this);
+        start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Child thread: " + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Child interrupted.");
+        }
+        System.out.println("Exiting child thread.");
+    }
+}
+
+public class ExtendThread {
+    public static void main(String[] args) {
+        new NewThread2();
+
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Main thread: " + i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted.");
+        }
+        System.out.println("Main thread exiting");
+    }
+}

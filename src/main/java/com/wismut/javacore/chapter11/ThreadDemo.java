326ec9278ae05096739d9f5da4814dc16f7fab01
Wismut <wismutmax@gmail.com>
2020-05-01 20:40:06 +0300

    Implements CurrentThreadDemo Implements ThreadDemo

---
+++ src/main/java/com/wismut/javacore/chapter11/ThreadDemo.java
@@ -0,0 +1,40 @@
+package main.java.com.wismut.javacore.chapter11;
+
+class NewThread implements Runnable {
+    Thread t;
+
+    NewThread() {
+        t = new Thread(this, "Demo Thread");
+        System.out.println("Child thread: " + t);
+        t.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Child thread: " + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Child interrupted.");
+        }
+        System.out.println("Exiting child thread.");
+    }
+}
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        new NewThread();
+
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Main thread: " + i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted.");
+        }
+        System.out.println("Main thread exiting");
+    }
+}

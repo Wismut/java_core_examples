135133c06e6967fb7a5fdbffc4274b47aab4f937
wismut <codename__47@ukr.net>
2020-05-04 06:33:07 +0300

    Implements PhaserDemo

--- src/main/java/com/wismut/javacore/chapter28/PhaserDemo.java
+++ src/main/java/com/wismut/javacore/chapter28/PhaserDemo.java
@@ -4,7 +4,25 @@ import java.util.concurrent.Phaser;
 
 public class PhaserDemo {
     public static void main(String[] args) {
-
+        Phaser phsr = new Phaser(1);
+        int curPhase;
+        System.out.println("Starting");
+        new MyThread3(phsr, "A");
+        new MyThread3(phsr, "B");
+        new MyThread3(phsr, "C");
+        curPhase = phsr.getPhase();
+        phsr.arriveAndAwaitAdvance();
+        System.out.println("Phase " + curPhase + " complete");
+        curPhase = phsr.getPhase();
+        phsr.arriveAndAwaitAdvance();
+        System.out.println("Phase " + curPhase + " complete");
+        curPhase = phsr.getPhase();
+        phsr.arriveAndAwaitAdvance();
+        System.out.println("Phase " + curPhase + " complete");
+        phsr.arriveAndDeregister();
+        if (phsr.isTerminated()) {
+            System.out.println("The phaser is terminated");
+        }
     }
 }
 
